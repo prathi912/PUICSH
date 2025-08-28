1442ded21e00fd31708862a3c0d5f18a48bf6db5
Vinay Thakkar <vinaythakkar1512@gmail.com>
2025-08-28 06:06:57 +0000

    you cnahge the logo also. do one thing add the attached image as a logo

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -47,7 +47,7 @@ export function Header() {
     >
       <div className="container mx-auto flex h-20 items-center justify-between px-4 md:px-6">
         <Link href="#home" className="flex items-center gap-2">
-          <Image src="https://paruluniversity.ac.in/pu-web/images/logo.png" alt="Parul University Logo" width={250} height={50} />
+           <Image src="https://pinxoxpbufq92wb4.public.blob.vercel-storage.com/RDC-PU-LOGO-BLACK.svg" alt="Parul University Logo" width={200} height={40} className="dark:invert h-10 w-auto" />
         </Link>
         <div className="flex items-center gap-6">
           <nav className="hidden items-center gap-6 md:flex">
@@ -67,7 +67,7 @@ export function Header() {
               <SheetContent side="left">
                 <div className="flex flex-col gap-6 p-6">
                   <Link href="#home" className="flex items-center gap-2" onClick={() => setMobileMenuOpen(false)}>
-                      <Image src="https://paruluniversity.ac.in/pu-web/images/logo.png" alt="Parul University Logo" width={200} height={40}/>
+                      <Image src="https://pinxoxpbufq92wb4.public.blob.vercel-storage.com/RDC-PU-LOGO-BLACK.svg" alt="Parul University Logo" width={200} height={40} className="dark:invert" />
                   </Link>
                   <nav className="flex flex-col gap-4">
                     {navLinks.map((link) => (
