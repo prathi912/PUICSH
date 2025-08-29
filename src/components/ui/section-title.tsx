a38948b52f5faa090952bab385d8db78978cf62d
PRANAV PRAMOD RATHI <210306105088@paruluniversity.ac.in>
2025-08-29 10:50:46 +0000

    remove the lines from the title of componenetns

--- src/components/ui/section-title.tsx
+++ src/components/ui/section-title.tsx
@@ -6,16 +6,8 @@ interface SectionTitleProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export function SectionTitle({ children, className, ...props }: SectionTitleProps) {
   return (
-    <div className={cn("flex items-center gap-4", className)} {...props}>
-      <div className="hidden sm:flex flex-grow w-full items-center justify-end">
-        <div className="h-0.5 bg-primary w-16"></div>
-        <div className="h-0.5 bg-accent w-32 mt-1 mr-[-4rem]"></div>
-      </div>
+    <div className={cn("flex items-center justify-center gap-4", className)} {...props}>
       <h2 className="text-3xl font-bold font-headline whitespace-nowrap text-center">{children}</h2>
-      <div className="hidden sm:flex flex-grow w-full items-center">
-        <div className="h-0.5 bg-primary w-16"></div>
-        <div className="h-0.5 bg-accent w-32 mt-1 ml-[-4rem]"></div>
-      </div>
     </div>
   );
 }
